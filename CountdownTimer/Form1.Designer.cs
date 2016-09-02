cb9cd8892abeb6af383648bc3c9cd0eeb338767e
Justin Silva <JSilva@FiveSixTwo.com>
2016-09-02 15:44:03 -0700

    First

--- CountdownTimer/Form1.Designer.cs
+++ CountdownTimer/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.btnRemoveTime = new System.Windows.Forms.Button();
+            this.btnAddTime = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 63.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(34, 479);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(320, 140);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 159.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.Location = new System.Drawing.Point(148, 199);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(928, 241);
+            this.lblCountdown.TabIndex = 3;
+            this.lblCountdown.Text = "00:00:00";
+            this.lblCountdown.Click += new System.EventHandler(this.lblCountdown_Click);
+            // 
+            // btnRemoveTime
+            // 
+            this.btnRemoveTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 63.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveTime.Location = new System.Drawing.Point(962, 499);
+            this.btnRemoveTime.Name = "btnRemoveTime";
+            this.btnRemoveTime.Size = new System.Drawing.Size(114, 101);
+            this.btnRemoveTime.TabIndex = 5;
+            this.btnRemoveTime.Text = "-";
+            this.btnRemoveTime.UseVisualStyleBackColor = true;
+            this.btnRemoveTime.Click += new System.EventHandler(this.btnRemoveTime_Click);
+            // 
+            // btnAddTime
+            // 
+            this.btnAddTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 63.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTime.Location = new System.Drawing.Point(769, 499);
+            this.btnAddTime.Name = "btnAddTime";
+            this.btnAddTime.Size = new System.Drawing.Size(109, 101);
+            this.btnAddTime.TabIndex = 6;
+            this.btnAddTime.Text = "+";
+            this.btnAddTime.UseVisualStyleBackColor = true;
+            this.btnAddTime.Click += new System.EventHandler(this.btnAddTime_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 63.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(263, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(661, 139);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Band Name";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1125, 631);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAddTime);
+            this.Controls.Add(this.btnRemoveTime);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.btnReset);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.Button btnRemoveTime;
+        private System.Windows.Forms.Button btnAddTime;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CountdownTimer/Form1.cs
+++ CountdownTimer/Form1.cs
@@ -12,9 +12,97 @@ namespace CountdownTimer
 {
     public partial class Form1 : Form
     {
+
+        private TimeSpan ts = new TimeSpan();
+        private System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        private Timer timer;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            sw.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ts = ts.Subtract(new TimeSpan(0, 0, 1));
+            lblCountdown.Text = ts.ToString();
+
+
+
+            if (ts.TotalSeconds < 60)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else
+            {
+                this.BackColor = DefaultBackColor;
+
+            }
+
+            if (ts.TotalMinutes < 0)
+            {
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                this.BackColor = DefaultBackColor;
+            }
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = (10 * 100); // 1 secs
+            timer.Tick += new EventHandler(timer_Tick);
+            textBox1.Text = "Band Name";
+        }
+
+        private void btnRemoveTime_Click(object sender, EventArgs e)
+        {
+            ts = ts.Subtract(new TimeSpan(0, 15, 0));
+            lblCountdown.Text = ts.ToString();
+        }
+
+
+        private void btnAddTime_Click(object sender, EventArgs e)
+        {
+            ts = ts.Add(new TimeSpan(0, 15, 0));
+            lblCountdown.Text = ts.ToString();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+            timer.Stop();
+            timer.Enabled = false;
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            //sw.Restart();
+            timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = (10 * 100); // 1 secs
+            //timer.Tick += new EventHandler(timer_Tick);
+
+            ts = new TimeSpan(0, 0, 0);
+            lblCountdown.Text = ts.ToString();
+        }
+
+        private void lblCountdown_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
